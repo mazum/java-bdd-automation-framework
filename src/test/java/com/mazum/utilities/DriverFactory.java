563eba9bee322d0cc17d6a4643d4dffdea528c3a
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2017-09-13 23:28:36 +1000

    DriverFactory initial check in.

---
+++ src/test/java/com/mazum/utilities/DriverFactory.java
@@ -0,0 +1,10 @@
+package com.mazum.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Aby on 13/09/2017.
+ */
+public class DriverFactory {
+    public static WebDriver driver;
+}
